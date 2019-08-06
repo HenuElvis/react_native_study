5aed7fd85bc50e95e839894f93ce3f3582fdaf51
wangzheng1 <wangzheng1@lvmama.com>
2019-08-06 20:30:55 +0800

    'bottomTabBar添加红点'

--- src/tabPage/TabPage.js
+++ src/tabPage/TabPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View,Text,StyleSheet} from 'react-native';
-import {createBottomTabNavigator,createAppContainer} from 'react-navigation'
+import {createBottomTabNavigator,createAppContainer} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class A extends React.Component{
     render(){
@@ -51,7 +52,65 @@ const tabNavigator = createBottomTabNavigator({
     B:{
         screen:B
     }
-});
+},
+{
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'A') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+          // Sometimes we want to add badges to some icons. 
+          // You can check the implementation below.
+          IconComponent = HomeIconWithBadge; 
+        } else if (routeName === 'B') {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
+class IconWithBadge extends React.Component {
+    render() {
+      const { name, badgeCount, color, size } = this.props;
+      return (
+        <View style={{ width: 24, height: 24, margin: 5 }}>
+          <Ionicons name={name} size={size} color={color} />
+          { badgeCount > 0 && (
+            <View style={{
+              // If you're using react-native < 0.57 overflow outside of the parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}>
+              <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>{badgeCount}</Text>
+            </View>
+          )}
+        </View>
+      );
+    }
+  }
+
+const HomeIconWithBadge = (props) => {
+// You should pass down the badgeCount in some other ways like react context api, redux, mobx or event emitters.
+return <IconWithBadge {...props} badgeCount={3} />;  
+}
 const Tab = createAppContainer(tabNavigator);
 
 export default class TabPage extends React.Component{
