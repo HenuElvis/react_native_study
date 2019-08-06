a68bc3b1338fa1b8fff8494cdab1876c0baa6ad4
wangzheng1 <wangzheng1@lvmama.com>
2019-08-06 17:51:53 +0800

    '添加标题栏与页面属性交互例'

--- src/productDetail/ProductDetails.js
+++ src/productDetail/ProductDetails.js
@@ -2,13 +2,19 @@ import React from 'react'
 import {View,Button ,Text} from 'react-native'
 
 export default class ProductDetails extends React.Component{
+
     static navigationOptions=({navigation})=>{
         return {
             title:navigation.getParam("data",{key:'default product name'}).key,
-            headerTitle:<View><Text style ={{color:'white'}}>HeaderTitle</Text></View>,
-            headerRight:<View><Text style ={{color:'white'}}>HeaderRight</Text></View>
+            // headerTitle:<View><Text style ={{color:'white'}}>HeaderTitle</Text></View>,
+            headerRight:<View style={{marginRight:20}}><Text style ={{color:'yellow',fontSize:18}} onPress={navigation.getParam('increment')}>increment</Text></View>
         };
     }
+
+    state={
+        count:0
+    }
+
     render(){
         const item = this.props.navigation.getParam("data",{key:'default product name'});
 
@@ -23,14 +29,22 @@ export default class ProductDetails extends React.Component{
                 >parm:{item.key};点击更换title</Text>
 
                 <View style={{margin:50}}>
-                    <Button 
-                        title={'aaa'}
-                        onPress={()=>{
-                            alert('ceshi');
-                        }}
-                    ></Button>
+                   <Text style={{textAlign:'center'}}>count:{this.state.count}</Text>
                 </View>
             </View>
         );
+    }
+
+    componentDidMount(){
+        const {navigation} = this.props;
+        navigation.setParams({
+            increment:this.increment
+        });
+    }
+
+    increment = ()=>{
+        this.setState({
+            count:this.state.count+1
+        })
     }
 }
